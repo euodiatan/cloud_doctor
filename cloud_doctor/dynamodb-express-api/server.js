63388df0f016067fb4234bd6db8e490160f1b6be
euodiatan <euodiatqy@gmail.com>
2023-10-31 14:31:59 +0800

    security

--- cloud_doctor/dynamodb-express-api/server.js
+++ cloud_doctor/dynamodb-express-api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const AWS = require('aws-sdk');
 
 const app = express();
 
@@ -13,16 +14,9 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-const AWS = require('aws-sdk');
-
-// Configure AWS region and credentials
+// Configure AWS region
 AWS.config.update({
-  region: 'ap-southeast-2', 
-  accessKeyId:'AKIAQD7B7R2QOUQLDNQ3',
-  secretAccessKey:'lQrNYkkL9mKuXRWPxwDRmbQcBfkvXqwkGR/Z+VLp'// e.g., 'us-west-1'
-  // If you're running the app on your local machine, you might want to configure your credentials here
-  // accessKeyId: 'YOUR_ACCESS_KEY',
-  // secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
+  region: 'ap-southeast-2'
 });
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
