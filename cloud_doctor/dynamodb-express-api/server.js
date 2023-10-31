8b2e51c1e352caced13202a5e7f2c87e176f36c0
euodiatan <euodiatqy@gmail.com>
2023-10-31 15:45:17 +0800

    Update server.js

--- cloud_doctor/dynamodb-express-api/server.js
+++ cloud_doctor/dynamodb-express-api/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const AWS = require('aws-sdk');
+const path = require('path');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../build')));
 
 const PORT = 3001;
 
@@ -23,3 +25,6 @@ const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const itemsRoutes = require('./routes/items');
 
 app.use('/', itemsRoutes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
