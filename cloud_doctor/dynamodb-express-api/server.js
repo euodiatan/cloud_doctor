370cdad67a0f8ab8dc9713482a615df9f8abfffc
euodiatan <euodiatqy@gmail.com>
2023-11-01 12:54:45 +0800

    Update server.js

--- cloud_doctor/dynamodb-express-api/server.js
+++ cloud_doctor/dynamodb-express-api/server.js
@@ -12,8 +12,8 @@ app.use(express.static(path.join(__dirname, '../build')));
 
 const PORT = 3001;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
 
 // Configure AWS region
